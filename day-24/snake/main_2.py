6f98937cc2066555f51835e697915def3ead77fc
cmurga95 <125426661+cmurga95@users.noreply.github.com>
2024-02-01 13:43:52 -0800

    update

---
+++ day-24/snake/main_2.py
@@ -0,0 +1,7 @@
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("../day-20/textfile.txt", mode = "r") as file:
+    contents = file.read()
+    print(contents)
