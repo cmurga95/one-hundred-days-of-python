30fe966223f706b2213ff22288d70fcd2103cba4
cmurga95 <125426661+cmurga95@users.noreply.github.com>
2024-03-04 14:37:27 -0800

    update-nopassnoemail

--- day-32/main.py
+++ day-32/main.py
@@ -1,11 +1,28 @@
 import smtplib
 
-my_email = "cmmurgav@hotmail.com"
-password = "uS-B8(ApJyY=r2C"
-connection = smtplib.SMTP("smtp.live.com")
-connection.starttls()
-connection.login(user = my_email, password = password)
-connection.sendmail(from_addr = my_email
-                    , to_addrs = "murga.carlos2013@gmail.com"
-                    , msg = "Hello")
 
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+
+#     connection.starttls()
+#     connection.login(user = my_email, password = password)
+#     connection.sendmail(from_addr = my_email
+#                         , to_addrs = "murga.carlos2013@gmail.com"
+#                         , msg = "Subject: Motivational stuff\n\nThis is the body of the email"
+#                         )
+#     # connection.close()
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+
+date_of_birth = dt.datetime(year = 1995, month = 12, day = 15)
+
+print(date_of_birth)
+
+with open("quotes.txt") as file:
+    data = [x for x in file]
+
+print(data[0])
